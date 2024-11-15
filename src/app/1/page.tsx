0f2347eca7dfa076d04822c0669c223a06f07c7a
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-11-15 17:10:20 +0100

    chore: style touch ups

--- src/app/1/page.tsx
+++ src/app/1/page.tsx
@@ -4,8 +4,8 @@ import { FaPinterest, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function One() {
   return (
-    <section className="flex flex-col items-center  rounded-2xl shadow-lg bg-white h-[32rem] w-[32rem] text-black">
-      <header className="relative w-full">
+    <section className="flex flex-col items-center rounded-2xl shadow-lg bg-white h-[32rem] w-[24rem] text-black gap-8 pb-12">
+      <header className="relative w-full flex items-center justify-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1440 320"
@@ -39,33 +39,35 @@ export default function One() {
             d="M0,32L40,53.3C80,75,160,117,240,133.3C320,149,400,139,480,117.3C560,96,640,64,720,69.3C800,75,880,117,960,165.3C1040,213,1120,267,1200,256C1280,245,1360,171,1400,133.3L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
           ></path>
         </svg>
-        <img
-          src="https://images.unsplash.com/photo-1668766418520-04797d637e78?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="profile"
-          className="rounded-full max-h-8 z-20"
-        />
+        <div className="rounded-full h-24 w-24 z-50 overflow-hidden mt-8">
+          <img
+            src="https://images.unsplash.com/photo-1668766418520-04797d637e78?q=80&w=2487&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="profile"
+            className="z-50 object-cover"
+          />
+        </div>
       </header>
 
-      <div className="pt-24 flex flex-col justify-center items-center ">
+      <div className="flex flex-col justify-center items-center ">
         <p className="text-2xl">Lorem Ipsum</p>
         <span className="text-sm text-black/60">@loremipsum</span>
       </div>
 
-      <div className="flex gap-4 px-12 pt-8">
+      <div className="flex gap-4 px-12">
         <FaXTwitter className="hover:cursor-pointer" />
         <FaLinkedin className="hover:cursor-pointer" />
         <FaPinterest className="hover:cursor-pointer" />
         <FaInstagram className="hover:cursor-pointer" />
       </div>
 
-      <div className="px-12 text-center tracking-tighter py-8">
+      <div className="px-12 text-center tracking-tighter">
         <p>
           Crafting brand and communication stratgies, creating visual designs,
           leading art direction and capturing portraits through photography.
         </p>
       </div>
 
-      <div className="flex gap-4 mt-auto py-12">
+      <div className="flex gap-4 mt-auto ">
         <a
           href=""
           className="px-12 py-3 bg-orange-300 rounded-2xl text-sm font-bold flex items-center"
